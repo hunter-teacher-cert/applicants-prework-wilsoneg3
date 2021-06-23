95bcfc3a0899afed7074f81c351bf9017d9c644b
Eric Wilson <84479785+wilsoneg3@users.noreply.github.com>
2021-06-23 01:01:47 +0000

    Added Pre03, tried to make a test loop run in Pre06... not succesful

--- Pre05/Methods.java
+++ Pre05/Methods.java
@@ -11,7 +11,7 @@ public class Methods {
           return false;
         }
 }
-    public boolean isTraingle(int a, int b, int c){
+  public boolean isTraingle(int a, int b, int c){
       if (a < (b+c) && b < (a+c) && c <(b+a)){
         return true;
       } else{
